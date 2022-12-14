349b08255eef91b13a82176dfb99eaa7ca6a995a
Shalfeei <Khusainovrenat@gmail.com>
2022-12-14 14:06:07 +0500

    add files

---
+++ 1sem/Проектный практикум/Лабораторная работа 3_2021/Лабораторная работа 3_2021/udplocal2csharp_vs11/udplocal2csharp/udplocal2/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace udplocal2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.textBoxReceive = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxRemote = new System.Windows.Forms.TextBox();
+            this.textBoxHostAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(12, 93);
+            this.textBoxSend.Multiline = true;
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(192, 113);
+            this.textBoxSend.TabIndex = 0;
+            // 
+            // textBoxReceive
+            // 
+            this.textBoxReceive.BackColor = System.Drawing.Color.Gainsboro;
+            this.textBoxReceive.Location = new System.Drawing.Point(210, 93);
+            this.textBoxReceive.Multiline = true;
+            this.textBoxReceive.Name = "textBoxReceive";
+            this.textBoxReceive.ReadOnly = true;
+            this.textBoxReceive.Size = new System.Drawing.Size(192, 113);
+            this.textBoxReceive.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Отправка сообщения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Извлечение сообщения";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(123, 34);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Запуск приложения";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(12, 238);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Отправить";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxRemote
+            // 
+            this.textBoxRemote.Location = new System.Drawing.Point(175, 238);
+            this.textBoxRemote.Name = "textBoxRemote";
+            this.textBoxRemote.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRemote.TabIndex = 4;
+            this.textBoxRemote.Text = "127.0.0.1";
+            // 
+            // textBoxHostAddress
+            // 
+            this.textBoxHostAddress.Location = new System.Drawing.Point(302, 238);
+            this.textBoxHostAddress.Name = "textBoxHostAddress";
+            this.textBoxHostAddress.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHostAddress.TabIndex = 4;
+            this.textBoxHostAddress.Text = "127.0.0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(299, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "хост-адрес";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(172, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "удаленный адрес";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(414, 275);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxHostAddress);
+            this.Controls.Add(this.textBoxRemote);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxReceive);
+            this.Controls.Add(this.textBoxSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Отправка и извлечение сообщений. Протокол UDP часть 2";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.TextBox textBoxReceive;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxRemote;
+        private System.Windows.Forms.TextBox textBoxHostAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 1sem/Проектный практикум/Лабораторная работа 3_2021/Лабораторная работа 3_2021/udplocal2csharp_vs11/udplocal2csharp/udplocal2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                        www.interestprograms.ru 
+//                      исходные коды программ и игр 
+//     ОТПРАВКА СООБЩЕНИЙ ПО ПРОТОКОЛУ UDP ЧАСТЬ 2 (подробное описание) 
+///////////////////////////////////////////////////////////////////////////////
+
+namespace udplocal2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            // Отправляем сообщение и  закрываем udpclient
+            UdpClient udp = new UdpClient();
+            byte[] message = Encoding.Default.GetBytes(textBoxSend.Text);
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(textBoxRemote.Text), 15000);
+            udp.Send(message, message.Length, ep);
+            udp.Close();
+        }
+ 
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Чтобы наше приложение не заблокировалось,
+            // для извлечения сообщений запускаем второй поток.
+            ThreadStart tstart = new ThreadStart(Receive);
+            workReceive = new Thread(tstart);
+            workReceive.Start();
+        }
+
+        bool stopReceive = false;
+        Thread workReceive = null;
+        UdpClient udp = null;
+        
+        // Функция запускаемая из дополниетльного потока
+        void Receive()
+        {
+            try
+            {
+                IPEndPoint ep = new IPEndPoint(IPAddress.Parse(textBoxHostAddress.Text), 15000);
+                udp = new UdpClient(ep);
+                while (true)
+                {
+                    IPEndPoint remote = null;
+                    byte[] message = udp.Receive(ref remote);
+                    ShowMessage(Encoding.Default.GetString(message) );
+                    if (stopReceive == true) break;
+                }
+            }
+            catch(Exception e)
+            {
+                //MessageBox.Show(e.Message);
+            }
+            finally
+            {
+               if (udp != null) udp.Close();
+            }
+                
+            
+        }
+
+        
+        // Специальный код доступа к свойствам объектов  из других потоков
+        delegate void SetTextCallback(string message);
+        void ShowMessage(string message)
+        {
+            if (this.textBoxReceive.InvokeRequired)
+            {
+                SetTextCallback dt = new SetTextCallback(ShowMessage);
+                this.Invoke(dt, new object[] { message });
+            }
+            else
+            {
+                this.textBoxReceive.Text = message;
+            }
+        }
+          
+        // Безопасный останов дополнительного потока
+        void StopReceive()
+        {
+            stopReceive = true;
+            if (udp != null) udp.Close();
+            if (workReceive != null) workReceive.Join();
+        }
+
+        // Перед закрытием формы остановим дополнительный поток
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopReceive();
+        }
+
+
+        
+    }
+}
